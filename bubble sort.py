81c351462c17de9ffa9ab41674bcbf0f0e9d0f97
Uttam patel <utampipaliya@gmail.com>
2022-11-07 16:58:41 +0530

    bubble sort

---
+++ bubble sort.py
@@ -0,0 +1,31 @@
+
+def bubble_sort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# def bubble_sort(list):
+# 	length = len(list) - 1
+# 	sorted = False
+# 	while not sorted:
+# 		sorted = True
+# 		for i in range(length):
+# 			if list[i] > list[i + 1]:
+# 				sorted = False
+# 				list[i], list[i + 1] = list[i + 1], list[i]
+# 	return list
+
+if __name__ == '__main__':
+    list = [5, 4, 3, 2, 1]
+    print(bubble_sort(list))
